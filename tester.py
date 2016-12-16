3ef109b54ff403efb2b3f1a108300a141f94f88b
raylu <lurayl@gmail.com>
2016-12-16 00:54:26 -0800

    improve tester to sleep and clean up
    the sleep in between connecting and requesting makes it very likely a
    connection will be open when the server execs

--- tester.py
+++ tester.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python3
 
 import signal
+import socket
 import subprocess
-import sys
 import threading
 import time
-import urllib.request
+
+keep_going = True
 
 def main():
+	global keep_going
 	server = subprocess.Popen(['./seamless.py'])
 	time.sleep(0.1)
+	threads = []
 	for _ in range(4):
 		t = threading.Thread(target=checker)
 		t.start()
+		threads.append(t)
 	print('waiting 1 second')
 	time.sleep(1)
 	print('sending HUP')
 	server.send_signal(signal.SIGHUP)
 	time.sleep(1)
-	print('done!')
+	print('tester cleaning up')
+	keep_going = False
+	for t in threads:
+		t.join()
 	server.terminate()
-	sys.exit()
+	time.sleep(1)
 
 def checker():
-	while True:
-		req = urllib.request.urlopen('http://localhost:8000')
-		assert req.read() == b'hello\n'
+	while keep_going:
+		with socket.create_connection(('localhost', 8000), 1) as sock:
+			time.sleep(0.1)
+			sock.sendall(b'GET / HTTP/1.1\r\nHost: localhost:8000\r\n\r\n')
+			data = sock.recv(1024)
+			assert data, 'got %r' % data
+			body = data.split(b'\r\n\r\n', 2)[1]
+			assert body == b'hello\n'
 
 if __name__ == '__main__':
 	main()
